94dee8c67694cb429d30ca690cde2ae537517b78
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2022-08-06 18:02:40 +0530

    Create balance_parentheses.py
    String to be checked can be passed at line number 21 while calling the function

---
+++ balance_parentheses.py
@@ -0,0 +1,22 @@
+def balance_parentheses(input_string):
+    open_brackets = '{[('
+    closing_brackets = '}])'
+    bracket_stack = list()
+    for val in input_string:
+        if val in open_brackets:
+            bracket_stack.append(val)
+        else:
+            if len(bracket_stack):
+                elem = bracket_stack[-1] 
+                if (elem == "{" and val == "}") or (elem == "[" and val == "]") or (elem == "(" and val == ")") :
+                    bracket_stack.pop()
+                else:
+                    return "Not balanced"
+            else:
+                return "Not balanced"
+    if bracket_stack:
+        return "Not balanced"
+    return "Balanced"
+
+print(balance_parentheses("[{}{}(]"))
+    
