7a324789ffcff8fb2603404eac0e6d0ee7caa72c
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2022-08-12 16:31:20 +0530

    Update balance_parentheses.py
    index solution added for balance parentheses

--- balance_parentheses.py
+++ balance_parentheses.py
@@ -2,7 +2,7 @@
 Check if the string contains balance parenthesis or not
 '''
 
-def balance_parentheses(input_string):
+def balance_parentheses_solution_one(input_string):
     open_brackets = '{[('
     bracket_stack = list()
     for val in input_string:
@@ -20,6 +20,28 @@ def balance_parentheses(input_string):
     if bracket_stack:
         return "Not balanced"
     return "Balanced"
-
-print(balance_parentheses("[{}{}(]"))
     
+
+def balance_parentheses_solution_two(input_string):
+    open_brackets = '{[('
+    closed_brackets = '}])'
+    bracket_stack = list()
+    for val in input_string:
+        if val in open_brackets:
+            bracket_stack.append(val)
+        else:
+            if len(bracket_stack):
+                elem = bracket_stack[-1]
+                open_index = open_brackets.index(elem)
+                close_index = closed_brackets.index(val)
+                if close_index == open_index :
+                    bracket_stack.pop()
+                else:
+                    return "Not balanced"
+            else:
+                return "Not balanced"
+    if bracket_stack:
+        return "Not balanced"
+    return "Balanced"
+
+print(balance_parentheses_solution_two("[{}{}]"))
