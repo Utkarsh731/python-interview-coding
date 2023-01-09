a1e091e6ed3788ed56065b28fcbd6a4cc7e34d24
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-09 11:08:59 +0530

    right angle traingle added

---
+++ right_angle_triangle.py
@@ -0,0 +1,8 @@
+def create_triangle(number_of_rows):
+    if not number_of_rows:
+        print("Please enter a number")
+    else:
+        for num in range(number_of_rows):
+            print("*"*(num+1))
+
+create_triangle(10)
