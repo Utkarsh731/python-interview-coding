d912028b83d5e9dc005394092ce4e1ea5b2ee399
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-16 11:20:03 +0530

    diamond pattern code optimised and comments added

--- diamond_pattern.py
+++ diamond_pattern.py
@@ -1,28 +1,36 @@
 '''
 Write a code to print the diamond shape:
-input =4 
+input =4
 output =
-   * 
-  * * 
- * * * 
-* * * * 
-* * * * 
- * * * 
-  * * 
-   * 
+   *
+  * *
+ * * *
+* * * *
+* * * *
+ * * *
+  * *
+   *
 
 '''
+# This code prints a diamond shape of n rows
+n = int(input("Enter the number of rows: "))
+c = n - 1
 
-n=int(input("enter the num of rows"))
-c=n-1
-for i in range(1,n+1):
-    print(" "*c,end="")
-    c-=1
-    for j in range(0,i):
-        print("*",end=" ")
+# Upper half of the diamond
+for i in range(1, n + 1):
+    # Print spaces to align the shape
+    print(" " * c, end="")
+    c -= 1
+    # Print stars to form the shape
+    for j in range(0, i):
+        print("*", end=" ")
     print()
-for i in range(0,n):
-    print(" "*i,end="")
-    for j in range(i,n):
-        print("*",end=" ")
+
+# Lower half of the diamond
+for i in range(0, n):
+    # Print spaces to align the shape
+    print(" " * i, end="")
+    # Print stars to form the shape
+    for j in range(i, n):
+        print("*", end=" ")
     print()
