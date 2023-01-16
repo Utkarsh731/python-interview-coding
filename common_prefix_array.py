f11454b3251e3e20fb635063c630de1523683cfa
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-16 11:16:29 +0530

    check common prefix array code optimised and comments added

--- common_prefix_array.py
+++ common_prefix_array.py
@@ -1,24 +1,20 @@
-'''
-Given an array of strings, return the common prefixes, 
-if not found return an empty array. Ex. ['abcd', 'abb', 'acd'] -> return 'a'
-'''
+# Given an array of strings, return the common prefixes, if not found return an empty string.
+# Ex. ['abcd', 'abb', 'acd'] -> return 'a'
 
 def common_prefix(arr):
-    if not len(arr):
+    if not len(arr): # check if the array is empty
         return ""
-    if len(arr) == 1:
+    if len(arr) == 1: # check if the array has only one element
         return arr[0]
-    arr.sort()
-    first_elem = arr[0]
-    last_elem = arr[len(arr)-1]
+    arr.sort() # Sort the array
+    first_elem = arr[0] # Get the first element of the array
+    last_elem = arr[-1] # Get the last element of the array
     prefix = ""
-    for i in range(0, len(first_elem)):
-        if first_elem[i] == last_elem[i]:
-            prefix += first_elem[i]
+    for i in range(min(len(first_elem), len(last_elem))):  # Iterate through the smallest length of the first and last element
+        if first_elem[i] == last_elem[i]:  # Check if the characters at the current position match
+            prefix += first_elem[i]  # If they match, add the character to the prefix
         else:
             break
     return prefix
 
-
-
-print(common_prefix(['abcd', 'abb', 'abcd']))
+print(common_prefix(['abcd', 'abb', 'abcd']))  # 'ab'
