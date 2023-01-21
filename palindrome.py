0f53ae1b6b25190f7653e79bece4383ed7513256
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2023-01-21 18:28:36 +0530

    Update palindrome.py
    comments added

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,18 @@
-'''
-Given a string, check if it is a palindrome by ignoring spaces. E.g. race car would be a palindrome.
-'''
-
 def check_palidrome(word):
+    """
+    This function takes a string as input and checks if it is a palindrome by ignoring spaces.
+    """
+    # Removing spaces from the input word
     word = word.replace(" ","")
+    #Checking if the length of the word is odd
     if len(word)%2:
+        # Reversing the word
         reverse_word = word[::-1]
+        # Checking if the reversed word is same as the original word
         if word == reverse_word:
             return "It is a palindrome"
     return "Not a palindrome"
 
 
-
+# test case
 print(check_palidrome("race car"))
