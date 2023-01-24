a155bce6183162177fa4e3e53a2a6e973c540b31
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-24 10:55:11 +0530

    comments added

--- product_array.py
+++ product_array.py
@@ -1,18 +1,19 @@
-'''
-You are given an array of integers. Return an array of the same size where the element at each index 
-is the product of all the elements in the original array except for the element at that index. 
-For example, an input of [1, 2, 3, 4, 5] should return [120, 60, 40, 30, 24]. You cannot use division in this problem.
-'''
-
 def product_array(array):
+    """
+    This function takes an array of integers and returns an array of the same size where the element at each index is the product of all the elements in the original array except for the element at that index.
+    """
     output_array = []
+    #iterating through the array
     for i in range(0, len(array)):
         num=1
+        #calculating the product of all elements except the current element
         for x in array:
             if x!=array[i]:
                 num=x*num
+        #append the product to the output array
         output_array.append(num)
     return output_array
 
 
+#test case
 print(product_array([1, 2, 3, 4, 5]))
