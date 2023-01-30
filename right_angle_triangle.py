f0ed55acd36ef5462af33ecb150a97abb7d75979
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-30 11:25:40 +0530

    comments added

--- right_angle_triangle.py
+++ right_angle_triangle.py
@@ -1,8 +1,12 @@
+# The function creates a triangle of asterisks with the number of rows specified by the input argument
 def create_triangle(number_of_rows):
+    # If no value is entered for the number of rows, a prompt to enter a number is displayed
     if not number_of_rows:
         print("Please enter a number")
     else:
+        # A for loop is used to create the triangle by printing asterisks in increasing order for each row
         for num in range(number_of_rows):
             print("*"*(num+1))
 
+# Call the function and pass an argument of 10
 create_triangle(10)
